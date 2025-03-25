3c827b1964484fe02baa9f29f4d07ba1e984a7f2
mukund112003 <mukund.binstellar@gmail.com>
2025-03-25 18:18:16 +0530

    add Search featur in My Future screen

---
+++ lib/screens/my_futures/animation.dart
@@ -0,0 +1,68 @@
+
+import 'package:flutter/material.dart';
+
+class ManualAnimatedUserTile extends StatefulWidget {
+  final Widget child;
+  final int index;
+
+  const ManualAnimatedUserTile({
+    super.key,
+    required this.child,
+    required this.index,
+  });
+
+  @override
+
+  _ManualAnimatedUserTileState createState() => _ManualAnimatedUserTileState();
+}
+
+class _ManualAnimatedUserTileState extends State<ManualAnimatedUserTile>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.1),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    // Stagger the animation based on index.
+    Future.delayed(Duration(milliseconds: widget.index * 100), () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _opacityAnimation,
+      child: SlideTransition(
+        position: _slideAnimation,
+        child: widget.child,
+      ),
+    );
+  }
+}
--- lib/screens/my_futures/my_future_capusles.dart
+++ lib/screens/my_futures/my_future_capusles.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:future_capsule/core/constants/colors.dart';
-import 'package:future_capsule/core/images/images.dart';
 import 'package:future_capsule/core/widgets/shimmer_effect/user_tile_shimmer.dart';
 import 'package:future_capsule/data/controllers/recipients.controller.dart';
 import 'package:future_capsule/data/models/capsule_modal.dart';
 import 'package:future_capsule/data/models/user_modal.dart';
+import 'package:future_capsule/screens/my_futures/animation.dart';
 import 'package:future_capsule/screens/my_futures/my_future_capsule_view.dart';
 import 'package:future_capsule/screens/my_futures/my_future_tile.dart';
 import 'package:get/get.dart';
@@ -21,90 +21,162 @@ class _MyFutureCapsulesState extends State<MyFutureCapsules> {
   final RecipientController _recipientController =
       Get.put(RecipientController());
 
+  String? searchKeyword;
+  late final Stream<Map<String, List<Map<String, dynamic>>>>
+      sharedCapsulesStream;
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
     // Start listening to the stream
-    _recipientController.fetchSharedCapsulesWithUsersOPTStream().listen((data) {
-      setState(() {}); // Update UI whenever new data is received
-    });
+    sharedCapsulesStream =
+        _recipientController.fetchSharedCapsulesWithUsersOPTStream();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.dDeepBackground,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: AppColors.dDeepBackground,
-        title: Row(
-          children: [
-            const Text(
-              "My Future",
-              style: TextStyle(
-                  color: AppColors.dNeonCyan, fontWeight: FontWeight.w700),
-            ),
-            const SizedBox(width: 8),
-            Image.asset(AppImages.capsules, height: 40),
-          ],
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.filter_alt_rounded,
-                color: AppColors.dSoftGrey),
-          ),
-        ],
-      ),
-      body: StreamBuilder<Map<String, List<Map<String, dynamic>>>>(
-        stream: _recipientController.fetchSharedCapsulesWithUsersOPTStream(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return ListView.builder(
-                itemCount: 5, itemBuilder: (c, i) => const UserTileShimmer());
-          }
-
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-                child: Text("No capsules found",
-                    style: TextStyle(color: Colors.white)));
-          }
-
-          final myFuture = snapshot.data!;
-          final userList = _recipientController.myFutureUserList;
-
-          return ListView.builder(
-            itemCount: userList.length,
-            itemBuilder: (context, index) {
-              final UserModel user = userList[index];
-              final userCapsules = myFuture[user.userId] ?? [];
-
-              if (userCapsules.isEmpty) return const SizedBox.shrink();
-
-              final List<CapsuleModel> capsules = userCapsules
-                  .map((cap) => CapsuleModel.fromJson(
-                      cap['data'] as Map<String, dynamic>))
-                  .toList();
-
-              final List<String> dateList = userCapsules
-                  .map((data) => data['sharedDate'].toString())
-                  .toList();
-
-              return GestureDetector(
-                onTap: () {
-                  Get.to(() => MyFutureCapsuleView(
-                      capsules: capsules, user: user, date: dateList));
-                },
-                child: MyFutureTile(
-                  user: user,
-                  lastDate: dateList.first.toDate()!,
-                  capsule: capsules.first,
+    return GestureDetector(
+      onTap: () => searchKeyword,
+      child: Scaffold(
+          backgroundColor: AppColors.dDeepBackground,
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: AppColors.dDeepBackground,
+            title: const Row(
+              children: [
+                Text(
+                  "My Future",
+                  style: TextStyle(
+                      color: AppColors.dNeonCyan, fontWeight: FontWeight.w700),
                 ),
-              );
-            },
-          );
-        },
-      ),
+              ],
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.filter_alt_rounded,
+                    color: AppColors.dSoftGrey),
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _searchController,
+                  style: const TextStyle(
+                      color: AppColors.kWhiteColor,
+                      fontWeight: FontWeight.w600),
+                  cursorColor: AppColors.dActiveColorPrimary,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    prefixIconColor: AppColors.kWhiteColor,
+                    hintText: 'Search...',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide: const BorderSide(
+                            color: AppColors.dActiveColorPrimary)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide: const BorderSide(
+                            color: AppColors.dActiveColorPrimary)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide: const BorderSide(
+                            color: AppColors.dActiveColorPrimary)),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      searchKeyword = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: StreamBuilder<Map<String, List<Map<String, dynamic>>>>(
+                  stream: sharedCapsulesStream,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return ListView.builder(
+                          itemCount: 5,
+                          itemBuilder: (c, i) => const UserTileShimmer());
+                    }
+
+                    if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(
+                        child: Text("No capsules found",
+                            style: TextStyle(color: Colors.white)),
+                      );
+                    }
+
+                    final myFuture = snapshot.data!;
+                    final userList = _recipientController.myFutureUserList;
+                    final filteredUserList =
+                        searchKeyword != null && searchKeyword!.isNotEmpty
+                            ? userList
+                                .where((user) => user.name
+                                    .toLowerCase()
+                                    .contains(searchKeyword!.toLowerCase()))
+                                .toList()
+                            : userList;
+
+                    if (filteredUserList.isEmpty) {
+                      return const Center(
+                        child: Text("No matching users found",
+                            style: TextStyle(color: Colors.white)),
+                      );
+                    }
+
+                    return ListView.builder(
+                      itemCount: filteredUserList.length,
+                      itemBuilder: (context, index) {
+                        final UserModel user = filteredUserList[index];
+                        final userCapsules = myFuture[user.userId] ?? [];
+
+                        if (userCapsules.isEmpty) {
+                          return const SizedBox.shrink();
+                        }
+
+                        final List<CapsuleModel> capsules = userCapsules
+                            .map((cap) => CapsuleModel.fromJson(
+                                cap['data'] as Map<String, dynamic>))
+                            .toList();
+
+                        final List<String> dateList = userCapsules
+                            .map((data) => data['sharedDate'].toString())
+                            .toList();
+
+                        return ManualAnimatedUserTile(
+                          index: index,
+                          child: GestureDetector(
+                            onTap: () {
+
+                              Get.to(() => MyFutureCapsuleView(
+                                  capsules: capsules,
+                                  user: user,
+                                  date: dateList));
+                              searchKeyword = "";
+                              _searchController.value =
+                                  const TextEditingValue(text: "");
+                              FocusScope.of(context).unfocus();
+                              setState(() {});
+                            },
+                            child: MyFutureTile(
+                              user: user,
+                              lastDate: dateList.first.toDate()!,
+                              capsule: capsules.first,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
